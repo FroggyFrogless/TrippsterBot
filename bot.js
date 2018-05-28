6d6170d327a076d2fedae19fd6f39ff02046fd3e
FroggyFrogless <SlimerzKing@gmail.com>
2018-05-27 21:55:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ client.on('message', message => {
   if (!message.guild) return;
 
   // If the message content starts with "!kick"
-  if (message.content.startsWith('!kick')) {
+  if (message.content.startsWith('~~kick')) {
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();
@@ -88,7 +88,7 @@ client.on('message', message => {
   if (!message.guild) return;
 
   // if the message content starts with "!ban"
-  if (message.content.startsWith('!ban')) {
+  if (message.content.startsWith('~~ban')) {
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();

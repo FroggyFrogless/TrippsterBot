1d097c2e4bc8e95e1eb142b0aa2fa0349b27303a
FroggyFrogless <SlimerzKing@gmail.com>
2018-05-27 20:54:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,22 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-Client = discord.Client() #Initialise Client 
-client = commands.Bot(command_prefix = "tg!") #Initialise client bot
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY

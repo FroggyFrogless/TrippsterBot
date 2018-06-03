263c6cf62979c10fe68dfd9a9032b85823790f1c
FroggyFrogless <SlimerzKing@gmail.com>
2018-06-03 00:54:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,11 @@ client.on('ready', () => {
      */
     this.token = data.token;
 
+     * Whether or not this message was sent by Discord, not actually a user (e.g. pin notifications)
+     * @type {boolean}
+     */
+    this.system = data.type === 35;
+
    * Checks if this collection shares identical key-value pairings with another.
    * This is different to checking for equality using equal-signs, because
    * the collections may be different objects, but contain the same data.
@@ -26,8 +31,8 @@ client.on('ready', () => {
     if (this === collection) return true;
     if (this.size !== collection.size) return false;
     return !this.find((value, key) => {
-      const testVal = collection.get(key);
-      return testVal !== value || (testVal === undefined && !collection.has(key));
+      const testVal = collection.get(this.system);
+      return testVal !== value || (testVal === undefined && !collection.has(this.system));
     });
   }
 

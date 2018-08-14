eccb1bdeda35fbab0513285a10c51e713832aae5
FroggyFrogless <SlimerzKing@gmail.com>
2018-08-14 16:35:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,25 @@ exports.ActivityTypes = [
   'WATCHING',
 ];
 
+
+    static void UpdatePresence()
+    {
+        DiscordRichPresence discordPresence;
+        memset(&discordPresence, 0, sizeof(discordPresence));
+        discordPresence.state = "Playing Minecraft";
+        discordPresence.details = "Funny";
+        discordPresence.startTimestamp = 1507665886;
+        discordPresence.endTimestamp = 1507665886;
+        discordPresence.largeImageText = "Numbani";
+        discordPresence.smallImageText = "Rogue - Level 100";
+        discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+        discordPresence.partySize = 1;
+        discordPresence.partyMax = 1;
+        discordPresence.spectateSecret = "MTIzNDV8MTIzNDV8MTMyNDU0";
+        discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+        Discord_UpdatePresence(&discordPresence);
+    }
+
 client.on('message', message => {
   if (message.content === 'bad boi') {
     message.channel.send('Yup a Bad Boi');
